971d00be60f431c025cdf8b492c17cc02c64854d
Yogesh <35971073+yogeshd278@users.noreply.github.com>
2026-09-11 00:35:55 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        
+        unordered_set<int> seen;
+    
+        for (int i = 0; i < nums.size(); i++) {
+            if(seen.find(nums[i]) != seen.end()) {
+                return true;
+            }
+            seen.insert(nums[i]);
+        }
+        return false;
+    }
+};
